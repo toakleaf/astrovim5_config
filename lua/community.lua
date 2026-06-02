42f021095fa65582d16cc7256cce73f5da898f78
Tyler <toakleaf@kayak.com>
2026-06-02 10:06:37 -0400

    fixed some stuff

--- lua/community.lua
+++ lua/community.lua
@@ -8,7 +8,6 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.colorscheme.gruvbox-nvim" },
-  { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.go" },
--- lua/polish.lua
+++ lua/polish.lua
@@ -1,5 +1,11 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- This will run last in the setup process.
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
+
+local view = {}
+vim.api.nvim_create_autocmd("BufWritePre", {
+  callback = function() view = vim.fn.winsaveview() end,
+})
+vim.api.nvim_create_autocmd("BufWritePost", {
+  callback = function() vim.fn.winrestview(view) end,
+})
